71c5fe2ab1ca44be2762d60082f2e4b502a06730
Rick South <rick@natamus.com>
2024-07-28 07:42:34 +0200

    (v3.3) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/betterconduitplacement/util/Reference.java
+++ Common/src/main/java/com/natamus/betterconduitplacement/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.betterconduitplacement.util;
 public class Reference {
 	public static final String MOD_ID = "betterconduitplacement";
 	public static final String NAME = "Better Conduit Placement";
-	public static final String VERSION = "3.2";
+	public static final String VERSION = "3.3";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
