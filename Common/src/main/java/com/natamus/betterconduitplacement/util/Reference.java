feee499415efb66e9a76d281ff7cbb85f70ab986
Rick South <rick@natamus.com>
2024-08-09 08:33:19 +0200

    Better Conduit Placement source code for 1.21.1.

---
+++ Common/src/main/java/com/natamus/betterconduitplacement/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.betterconduitplacement.util;
+
+public class Reference {
+	public static final String MOD_ID = "betterconduitplacement";
+	public static final String NAME = "Better Conduit Placement";
+	public static final String VERSION = "3.3";
+	public static final String ACCEPTED_VERSIONS = "[1.21.1]";
+}
