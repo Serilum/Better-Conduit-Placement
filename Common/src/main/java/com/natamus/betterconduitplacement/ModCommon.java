67a921d4c95202493deb2ed58cda4604a5c14d38
Rick South <rick@natamus.com>
2024-10-24 20:13:17 +0200

    Better Conduit Placement source code for 1.21.2.

---
+++ Common/src/main/java/com/natamus/betterconduitplacement/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.betterconduitplacement;
+
+import com.natamus.betterconduitplacement.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
