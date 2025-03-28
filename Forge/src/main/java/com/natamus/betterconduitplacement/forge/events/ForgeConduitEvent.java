ad0cf88d0300f1e245de03056bebd38fd159851c
Rick South <rick@natamus.com>
2025-03-28 01:00:47 +0100

    Better Conduit Placement source code for 1.21.5.

---
+++ Forge/src/main/java/com/natamus/betterconduitplacement/forge/events/ForgeConduitEvent.java
@@ -0,0 +1,32 @@
+package com.natamus.betterconduitplacement.forge.events;
+
+import com.natamus.betterconduitplacement.events.ConduitEvent;
+import com.natamus.collective.functions.WorldFunctions;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeConduitEvent {
+	@SubscribeEvent
+	public static void onWaterClick(PlayerInteractEvent.RightClickItem e) {
+		ConduitEvent.onWaterClick(e.getEntity(), e.getLevel(), e.getHand());
+	}
+	
+	@SubscribeEvent
+	public static void onConduitClick(PlayerInteractEvent.RightClickBlock e) {
+		if (!ConduitEvent.onConduitClick(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec())) {
+			e.setCanceled(true);
+		}
+	}
+	
+	@SubscribeEvent
+	public static void onBlockBreak(BlockEvent.BreakEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+		
+		ConduitEvent.onBlockBreak(level, e.getPlayer(), e.getPos(), e.getState(), null);
+	}
+}
